fe1a1062d1ab3e5371748f4df7d3ed8394fa5162
GüémezDiego <jesusapodacaatondo@hotmail.com>
2017-06-02 15:01:05 -0600

    abc termnalo

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,8 +1,31 @@
 var iniciaApp = function(){
-	//alert("Hola App");
+	//alert("hola Ripardo guapo PD:gracias");
 	var entrar=function(){
-		alert($("#txtUsuario").val());
-		alert($("#txtClave").val());
+		// alert($("#txtUsuario").val());
+		// alert($("#txtClave").val());
+		var usuario=$("#txtUsuario").val();
+		var clave  =$("#txtClave").val();
+		var parametros="opcion=valida"+
+						"&usuario="+usuario+
+						"&clave="+clave+
+						"&id="+Math.random();
+		var validaEntrada = $.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		validaEntrada.done(function(data){
+			alert(data.respuesta);
+			if(data.respuesta==true){
+				$("#datosUsuario").hide("slow");
+				$("nav").show("slow");
+				$("#secUsuarios").show("slow");
+			}
+		});
+		validaEntrada.fail(function(jqError,textStatus){
+			alert("Solicitud fallida: "+textStatus);
+		});
 	}
 	var teclaUsuario=function(tecla){
 		if(tecla.which==13){
@@ -14,9 +37,96 @@ var iniciaApp = function(){
 			entrar();
 		}
 	}
-	//Sección de declaración de eventos.
+	var datosUsuario=function(){
+		var usaurio=$("#txtNomUsuario").val();
+		var parametros="opcion=datosusuario"+
+						"&usuario="+usuario+
+						"&id="+Math.random();
+		var du=$.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		du.done(function(data){
+			if(data.respuesta==true){
+				$("#txtNomNombre").val(data.nombre);
+				$("#txtNomClave").val(data.clave);
+				$("#txtNomDepto").val(data.departamento);
+				$("#txtNomVigencia").val(data.vigencia);
+			}else{
+				$("#txtNomNombre").focus();
+			}
+		});
+		du.fail(function(jqError,textStatus){
+
+		});
+	}
+	var teclaNomUsuario=function(tecla){
+		if(tecla.which==13){
+			datosUsuario();
+		}
+	}
+	var altas=function(){
+		var usuario=$("#txtNomUsuario").val();
+		var nombre=$("#txtNomNombre").val();
+		var clave=$("#txtNomClave").val();
+		var depto=$("#txtNomDepto").val();
+		var vigencia=$("#txtNomVigencia").val();
+		var parametros="opcion=alta"+
+					   "&usuario="+usuario+
+					   "&nombre="+nombre+
+					   "&clave="+clave+
+					   "&departamento="+depto+
+					   "&vigencia="+vigencia+
+					   "&id="+Math.random();
+		var altaUsuario=$.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		altaUsuario.done(function(data){
+			if (data.respuesta==true) {
+				alert("Usuario dado de alta");
+			}else{
+				alert("Usuario existente o no se pudo registrar");
+			}
+		});
+		altaUsuario.fail(function(jqError,textStatus){
+
+		});
+
+	}
+	var bajas=function(){
+		var usuario=$("#txtNomUsuario").val();
+		var parametros="opcion=baja"+
+					   "&usuario="+usuario;
+		var bajaUsuario=$.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+		bajaUsuario.done(function(data){
+			if (data.respuesta==true) {
+				alert("Usuario dado de baja");
+			}else{
+				alert("Usuario inexistente");
+			}
+		});
+		bajaUsuario.fail(function(jqError,textStatus){
+
+		});
+
+	}
+	//sección de declaración de eventows
 	$("#btnEntrar").on("click",entrar);
 	$("#txtUsuario").on("keypress",teclaUsuario);
 	$("#txtClave").on("keypress",teclaClave);
+	$("#txtNomUsuario").on("keypress",teclaNomUsuario);
+	$("#btnAltas").on("click",altas);
+	$("#btnBajas").on("click",bajas);
+
 }
 $(document).ready(iniciaApp);
--- abc/php/datos.php
+++ abc/php/datos.php
@@ -3,23 +3,100 @@
 	function valida(){
 		$respuesta=false;
 		$conexion=conecta();
-		$u=GetSQLValueString($_GET["usuario"],"text");
-		$c=GetSQLValueString(md5($_GET["clave"]),"text");
-		$consulta=sprintf("select usuario, clave from usuarios where usuario=%s and clave=%s limit 1",$u,$c);
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		$c=GetSQLValueString(md5($_POST["clave"]),"text");
+		$consulta=sprintf("select usuario,clave 
+			               from usuarios 
+			               where usuario=%s and clave=%s 
+			               limit 1",$u,$c);
 		$resultado=mysql_query($consulta);
 		if(mysql_num_rows($resultado)>0){
 			$respuesta=true;
 		}
-		$salidaJSON = array('respuesta' => $respuesta );
+		$salidaJSON = array('respuesta' => $respuesta);
 		print(json_encode($salidaJSON));
 	}
-	//Menu principal
-	$opcion=$_GET["opcion"];
-	switch($opcion){
+	function datosUsuario(){
+		$respuesta=false;
+		$usuario=GetSQLValueString($_POST["usuario"],"text");
+		$conexion=conecta();
+		$consulta=sprintf("select * from usuarios where usuario=%s limit 1",%usuario);
+		$resultado=mysql_query(consulta);
+		$nombre="";
+		$clave="";
+		$departamento=0;
+		$vigencia=0;
+		if(mysql_num_rows($resultado)>0){
+			$respuesta=true;
+			if($registro=mysql_fetch_array($resultado)){
+				$nombre			=$registro["nombre"];
+				$clave			=$registro["clave"];
+				$departamento	=$registro["departamento"];
+				$vigencia		=$registro["vigencia"];
+			}
+
+		}	
+		$salidaJSON = array('respuesta' 	=> $respuesta,
+							'nombre' 		=> $nombre,
+							'clave'    		=> $clave,
+							'departamento'	=> $departamento,
+							'vigencia'    	=> $vigencia);
+	}
+	function alta(){
+		$respuesta=false;
+		$conexion=conecta();
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		$n=GetSQLValueString($_POST["nombre"],"text");
+		$c=GetSQLValueString(md5($_POST["clave"]),"text");
+		$d=GetSQLValueString($_POST["departamento"],"int");
+		$v=GetSQLValueString($_POST["vigencia"],"int");
+		//Buscar si existe
+		$busca=sprintf("select usuario from usuarios where usuario=%s",$u);
+		$resultadoBusca=mysql_query($busca);
+		if(mysql_num_rows($resultadoBusca)==0){//Si no existe
+			$inserta=sprintf("insert into usuarios values(default,%s,%s,%s,%d,%d)",$u,$n,$c,$d,$v);
+			mysql_query($inserta);
+			if(mysql_affected_rows()>0){
+			$respuesta=true;
+			}
+		}
+		$salidaJSON = array('respuesta' => $respuesta );
+		print json_encode($salidaJSON);
+	}
+	function baja(){
+		$respuesta=false;
+		$conexion=conecta();
+		$u=GetSQLValueString($_POST["usuario"],"text");
+		//Buscar si existe
+		$busca=sprintf("select usuario from usuarios where usuario=%s",$u);
+		$resultadoBusca=mysql_query($busca);
+		if(mysql_num_rows($resultadoBusca)>0){//Si no existe
+			$inserta=sprintf("delete into usuarios values(default,%s,%s,%s,%d,%d)",$u,$n,$c,$d,$v);
+			mysql_query($inserta);
+			if(mysql_affected_rows()>0){
+			$respuesta=true;
+			}
+		}
+		$salidaJSON = array('respuesta' => $respuesta );
+		print json_encode($salidaJSON);
+	}
+	//Menú principal
+	$opcion=$_POST["opcion"];
+	switch ($opcion) {
 		case 'valida':
 			valida();
+			break;	
+		case 'datousuario':
+			datoUsuario();		
+			break;
+		case 'alta':
+			alta();
+			break;
+		case 'baja':
+			baja();
 			break;
 		default:
+			# code...
 			break;
 	}
 ?>
